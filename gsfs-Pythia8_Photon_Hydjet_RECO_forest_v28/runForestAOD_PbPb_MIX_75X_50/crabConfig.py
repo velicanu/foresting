4297e72255c0016775a3b1e76279aeee70972700
Ran Bi <biran@mit.edu>
2016-03-08 20:48:52 +0100

    configs for gamma-jet samples

---
+++ gsfs-Pythia8_Photon_Hydjet_RECO_forest_v28/runForestAOD_PbPb_MIX_75X_50/crabConfig.py
@@ -0,0 +1,21 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'gsfs-Pythia8_Photon50_Hydjet_RECO_20160306_forest_v28_2'
+config.section_('JobType')
+config.JobType.psetName = 'runForestAOD_PbPb_MIX_75X.py'
+config.JobType.pluginName = 'Analysis'
+config.section_('Data')
+config.Data.inputDataset = '/Pythia8_Photon50_Hydjet_MB/gsfs-Pythia8_Photon50_Hydjet_RECO_20160305-a78b3072e46c33a0fe1fffcdcc303b7d/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 10
+config.Data.totalUnits = -1
+config.Data.outputDatasetTag = 'gsfs-Pythia8_Photon50_Hydjet_RECO_20160306_forest_v28_2'
+config.section_('User')
+config.section_('Site')
+config.Site.whitelist = ['T2_US_MIT']
+config.Site.storageSite = 'T2_US_MIT'
+# config.section_("Debug")
+# config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
---
+++ gsfs-Pythia8_Photon_pp_RECO_forest_v28/runForestAOD_pp_MC_75X_120/crabConfig.py
@@ -0,0 +1,21 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'gsfs-Pythia8_Photon120_pp_RECO_forest_v28'
+config.section_('JobType')
+config.JobType.psetName = 'runForestAOD_pp_MC_75X.py'
+config.JobType.pluginName = 'Analysis'
+config.section_('Data')
+config.Data.inputDataset = '/Pythia8_Photon120_pp502_TuneCUETP8M1/gsfs-Pythia8_Photon120_pp_RECO_20160306-0564587735dfa98972125c928a8975ef/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 10
+config.Data.totalUnits = -1
+config.Data.outputDatasetTag = 'gsfs-Pythia8_Photon120_pp_RECO_forest_v28'
+config.section_('User')
+config.section_('Site')
+config.Site.whitelist = ['T2_US_MIT']
+config.Site.storageSite = 'T2_US_MIT'
+# config.section_("Debug")
+# config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
