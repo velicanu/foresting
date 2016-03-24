7f41277c1a51c195f4033ad2f22b8fcc2cc06b98
Ran Bi <biran@mit.edu>
2016-03-24 16:32:57 +0100

    configs for official zjet mc foresting

---
+++ OfficialZJMC/pp/Pythia8_Zmu10mu10Jet/crabConfig.py
@@ -0,0 +1,21 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'Pythia8_Zmu10mu10Jet-HINppWinter16DR-75X_mcRun2_forest_v1'
+config.section_('JobType')
+config.JobType.psetName = 'runForestAOD_pp_MC_75X.py'
+config.JobType.pluginName = 'Analysis'
+config.section_('Data')
+config.Data.inputDataset = '/Pythia8_Zmu10mu10Jet/HINppWinter16DR-75X_mcRun2_asymptotic_ppAt5TeV_v3-v1/AODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
+config.Data.outputDatasetTag = 'Pythia8_Zmu10mu10Jet-HINppWinter16DR-75X_mcRun2_forest_v1'
+config.section_('User')
+config.section_('Site')
+#config.Site.whitelist = ['T2_US_MIT']
+config.Site.storageSite = 'T2_US_MIT'
+# config.section_("Debug")
+# config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
