8638fb73dd2b5f7e6992b066aa8ae64484db886f
Ran Bi <biran@mit.edu>
2016-03-24 15:49:33 +0100

    configs for official gamma-jet mc foresting

---
+++ OfficialGJMC/PbPb/AllQCDPhoton/runForestAOD_PbPb_MIX_75X_80/crabConfig.py
@@ -0,0 +1,21 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'Pythia8_Photon80_Hydjet_MB-HINPbPbWinter16DR-75X_mcRun2_HeavyIon_forest_v1'
+config.section_('JobType')
+config.JobType.psetName = 'runForestAOD_PbPb_MIX_75X.py'
+config.JobType.pluginName = 'Analysis'
+config.section_('Data')
+config.Data.inputDataset = '/Pythia8_Photon80_Hydjet_MB/HINPbPbWinter16DR-75X_mcRun2_HeavyIon_v13-v1/AODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = "LumiBased"
+config.Data.unitsPerJob = 12
+config.Data.totalUnits = -1
+config.Data.outputDatasetTag = 'Pythia8_Photon80_Hydjet_MB-HINPbPbWinter16DR-75X_mcRun2_HeavyIon_forest_v1'
+config.section_('User')
+config.section_('Site')
+#config.Site.whitelist = ['T2_US_MIT']
+config.Site.storageSite = 'T2_US_MIT'
+# config.section_("Debug")
+# config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
